2cf8fa36e3d63f30a4c32caad0d134d25014d235
Valtkf <valentin.vgregorio@gmail.com>
2024-07-25 12:51:43 +0200

    💄  F0 : Adding search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,9 +93,38 @@ export default function Home() {
             <Button variant="secondary">Ongoing</Button>
             <Button variant="link">Waiting for Confirmation</Button>
           </div>
+
           <NavigationMenu className="relative flex justify-start gap-4 list-none">
+            <form className="">
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full h-8 mt-4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search"
+                  required
+                />
+              </div>
+            </form>
             <NavigationMenuItem className="relative mt-4">
-              <NavigationMenuTrigger className="border-[1px] border-stone-400 px-4 py-2 text-slate-700">
+              <NavigationMenuTrigger className="h-8 border-[1px] border-stone-400 px-4 py-2 text-slate-700">
                 Object
               </NavigationMenuTrigger>
               <NavigationMenuContent className=" absolute left-0 top-full mt-2 w-full bg-white border border-stone-300 rounded shadow-lg">
@@ -111,7 +140,7 @@ export default function Home() {
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem className="relative mt-4">
-              <NavigationMenuTrigger className="border-[1px] border-stone-400 px-4 py-2 text-slate-700">
+              <NavigationMenuTrigger className="h-8 border-[1px] border-stone-400 px-4 py-2 text-slate-700">
                 Company
               </NavigationMenuTrigger>
               <NavigationMenuContent className=" absolute left-0 top-full mt-2 w-full bg-white border border-stone-300 rounded shadow-lg">
@@ -127,7 +156,7 @@ export default function Home() {
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem className="relative mt-4">
-              <NavigationMenuTrigger className="border-[1px] border-stone-400 px-4 py-2 text-slate-700">
+              <NavigationMenuTrigger className="h-8 border-[1px] border-stone-400 px-4 py-2 text-slate-700">
                 Statue
               </NavigationMenuTrigger>
               <NavigationMenuContent className=" absolute left-0 top-full mt-2 w-full bg-white border border-stone-300 rounded shadow-lg">
--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -3,13 +3,13 @@ import React from "react";
 interface ListItemProps {
   title: string;
   href: string;
-  children: React.ReactNode; // Utilisez React.ReactNode pour les enfants
+  children?: React.ReactNode; // Utilisez React.ReactNode pour les enfants, ici l'utilisation est otpionnelle
 }
 
 const ListItem: React.FC<ListItemProps> = ({ title, href, children }) => (
   <li className="list-none">
-    <a href={href} className="block p-2 border rounded hover:bg-gray-100">
-      <h3 className="font-semibold">{title}</h3>
+    <a href={href} className="block p-2 rounded hover:bg-gray-100">
+      <h3 className="font-normal text-sm text-stone-400">{title}</h3>
       <p>{children}</p>
     </a>
   </li>
