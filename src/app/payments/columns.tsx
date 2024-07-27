348c01063c671f2b9f37de0619f6e2d039f4df62
Valtkf <valentin.vgregorio@gmail.com>
2024-07-27 16:07:04 +0200

    🚧 F01 : rework of data table part and add logo file to add some svg images

--- src/app/payments/columns.tsx
+++ src/app/payments/columns.tsx
@@ -1,88 +1,194 @@
-import { Checkbox } from "@radix-ui/react-checkbox";
+import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 
-export type Payment = {
+import { Checkbox } from "@/components/ui/checkbox";
+import { ArrowDown } from "lucide-react";
+
+import { companies, Deal, Statue } from "@/store/store";
+import Button from "@/components/ui/button";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+/* export type Deals = {
   id: string;
+  add: Date;
   amount: number;
-  statue: "pending" | "processing" | "success" | "failed";
-  company: string;
   object: string;
-  add: boolean;
-  date: string;
-};
+  statue: "pending" | "cancelled" | "ongoing" | "waiting" | "completed";
+  company: {
+    name: string;
+    logo: JSX.Element;
+  };
+}; */
 
-const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
-};
-
-export const createColumns = (
-  handleAddChange: (id: string, value: boolean) => void
-): ColumnDef<Payment>[] => [
+export const columns: ColumnDef<Deal>[] = [
   {
     id: "select",
     header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => {
-          console.log("Select all:", value);
-          table.toggleAllPageRowsSelected(!!value);
-        }}
-        aria-label="Select all"
-        className="flex justify-center border-[1px] rounded-sm border-slate-400 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 w-4 h-4"
-      />
+      <div className="hidden lg:flex gap-[12px] lg:px-[24px] py-[12px]">
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      </div>
     ),
     cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => {
-          console.log("Select all:", value);
-          row.toggleSelected(!!value);
-        }}
-        id="default-checkbox"
-        aria-label="Select row"
-        className="flex justify-center border-[1px] rounded-sm border-slate-400 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 w-4 h-4"
-      />
+      <div className="hidden lg:flex gap-[12px] lg:px-[24px] py-[12px]">
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      </div>
     ),
     enableSorting: false,
     enableHiding: false,
   },
   {
     accessorKey: "add",
-    header: "Add",
-    cell: ({ row }) => (
-      <div className="flex items-center ">
-        <Checkbox
-          checked={row.getValue("add")}
-          onCheckedChange={(value) =>
-            handleAddChange(row.original.id, value === true)
-          }
-          aria-label="Add row"
-        />
-        <span className="">{row.original.date}</span>
-      </div>
-    ),
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="hidden xl:flex text-[#344054] font-medium text-[12px] leading-[18px] gap-[12px] lg:px-[24px] py-[12px] items-center"
+        >
+          Add
+          <ArrowDown className="h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const date = row.original.add;
+      const formattedDate = date.toLocaleDateString("en-EN", {
+        month: "short",
+        year: "2-digit",
+      });
+      return (
+        <div className="hidden xl:flex text-[#344054] font-medium text-[14px] gap-[12px] lg:px-[24px] py-[12px] items-center">
+          <span>{`${formattedDate.split(" ")[0]}. ${
+            formattedDate.split(" ")[1]
+          }`}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "object",
-    header: "Object",
+    header: () => (
+      <div className="hidden md:flex lg:px-[24px] py-[12px]">Object</div>
+    ),
+    cell: ({ row }) => {
+      const object = row.original.object;
+
+      return (
+        <div className="hidden md:flex text-[#344054] font-medium text-[12px] leading-[18px] gap-[12px] lg:px-[24px] py-[12px] items-center">
+          <span>{object}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "company",
-    header: "Company",
+    header: () => (
+      <div className="font-medium text-[12px] leading-[18px] lg:px-[24px] py-[12px]">
+        Company
+      </div>
+    ),
+    cell: ({ row }) => {
+      const companyName = row.original.company;
+      const companyLogo = companies
+        .find((company) => company.name === companyName)
+        ?.logo();
+      return (
+        <div className="flex text-[#344054] font-medium text-[14px] gap-[12px] lg:px-[24px] py-[12px] items-center">
+          <span>{companyLogo}</span> <span>{companyName}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "statue",
-    header: "Statue",
+    header: () => (
+      <div className="font-medium text-[12px] leading-[18px] lg:px-[24px] py-[12px]">
+        Statue
+      </div>
+    ),
+    cell: ({ row }) => {
+      const statue: Statue = row.original.statue;
+
+      let colorClass = "";
+      let statueFormatted = "";
+      switch (statue) {
+        case Statue.pending:
+          colorClass = "bg-[#ECB30A]";
+          statueFormatted = "Pending";
+          break;
+        case Statue.cancelled:
+          colorClass = "bg-[#EC0A0A]";
+          statueFormatted = "Cancelled";
+          break;
+        case Statue.ongoing:
+          colorClass = "bg-[#2AD730]";
+          statueFormatted = "Ongoing";
+          break;
+        case Statue.waiting:
+          colorClass = "bg-[#960AEC]";
+          statueFormatted = "Waiting for confirmation";
+          break;
+        default:
+          colorClass = "bg-[#0085FF]";
+          statueFormatted = "Completed";
+      }
+
+      return (
+        <div className="flex text-[#344054] font-medium text-[14px] gap-[12px] lg:px-[24px] py-[12px] items-center">
+          <span
+            className={cn(colorClass, "h-[6px] rounded-full w-[6px]")}
+          ></span>
+          <span>{statueFormatted}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
-    cell: ({ row }) => <span>{formatCurrency(row.getValue("amount"))}</span>,
+    header: () => (
+      <div className="font-medium text-[12px] leading-[18px] lg:px-[24px] py-[12px]">
+        Amount
+      </div>
+    ),
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      function addThousandsSeparator(amount: number) {
+        // Convertir le montant en chaîne de caractères
+        const amountStr = amount.toString();
+
+        // Vérifier si le montant contient plus de trois chiffres
+        if (amountStr.length > 3) {
+          // Séparer les chiffres avant et après le point
+          const beforePoint = amountStr.slice(0, -3);
+          const afterPoint = amountStr.slice(-3);
+
+          // Ajouter le point et concaténer les chiffres
+          return beforePoint + "." + afterPoint;
+        } else {
+          return amountStr;
+        }
+      }
+
+      const formattedAmount = addThousandsSeparator(amount);
+      const formatted = formattedAmount + " $USD";
+
+      return (
+        <div className="lg:px-[24px] py-[12px] text-[#344054] font-medium">
+          {formatted}
+        </div>
+      );
+    },
   },
 ];
